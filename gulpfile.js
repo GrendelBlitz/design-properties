653adc90c9a7f9882490df78d392bf3129992887
Sönke Rohde <soenke.rohde@gmail.com>
2014-11-11 16:42:56 -0800

    initial commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,4 @@
-design-properties
-=================
-
-## Setup
-
-    $ git clone https://github.com/salesforce-ux/design-properties.git
-    $ cd design-properties
-    $ npm install
-    $ gulp
-
-Please make sure to have [gulp.js](http://gulpjs.com/) installed globally.
-
-    $ sudo npm install -g gulp
-
-## License
-
+/*
 Copyright (c) 2014, salesforce.com, inc. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
@@ -23,3 +8,22 @@ Redistributions in binary form must reproduce the above copyright notice, this l
 Neither the name of salesforce.com, inc. nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+var gulp = require('gulp');
+var theo = require('theo');
+var del = require('del');
+var fs = require('fs');
+
+gulp.task('clean', function(done) {
+  del('./dist', done);
+});
+
+gulp.task('default', ['clean'], function() {
+  if(!fs.existsSync('./dist')){
+    fs.mkdirSync('./dist');
+  }
+  theo.convert('./variables/*.json', './dist', {
+    templates: ['styl', 'html', 'less', 'scss', 'ios.json', 'android.xml'],
+  });
+});
